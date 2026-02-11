5734a4fab5443d02e8809a3df61dc47307cfe966
JorgeIsaacVilla <jvilla10@outlook.es>
2026-02-11 14:00:52 -0500

    Se quitaron las imagenes del menú de las entradas

--- src/components/Body/ArrayMenu/Menu13.js
+++ src/components/Body/ArrayMenu/Menu13.js
@@ -1,11 +1,5 @@
 import React from "react";
 import "./Menu.css";
-/*import imagen1 from "./imgMenu13/img1.jpg";
-import imagen2 from "./imgMenu13/img2.jpg";
-import imagen3 from "./imgMenu13/img3.jpg";
-import imagen4 from "./imgMenu13/img4.jpg";*/
-
-import imageMaintenance from "../imgBody/imageMaintenance.png"
 
 function Menu13({sectionId, onProductClick}){
 
@@ -15,35 +9,20 @@ function Menu13({sectionId, onProductClick}){
 
     const menuItem =[
     {pos:1,
-    image: imageMaintenance,
     food:"Empanadas de Carne, Salsa y Pico de Gallo X 6 Und",
     price:10000,
     ingredients:""},
 
     {pos:2,
-    image: imageMaintenance,
     food:"Deditos de Queso y Salsa",
     price:12000,
     ingredients:""},
 
     {pos:3,
-    image: imageMaintenance,
     food:"Yuquita Frita",
     price:8000,
     ingredients:""},
-/* Verificar si aun se venden estos productos
-    {pos:5,
-    image: imageMaintenance,
-    food:"Platano Maduro X 1 Und",
-    price:5000,
-    ingredients:""},
 
-    {pos:6,
-    image: imageMaintenance,
-    food:"Mazorca Americana",
-    price:8000,
-    ingredients:"5 Porciones de Mazorca Bañadas en Salsa de la Casa y Queso Costeño."},
-*/
     ]
     return(
         <>
@@ -52,11 +31,6 @@ function Menu13({sectionId, onProductClick}){
                 {menuItem.map((item)=>
                 <li key= {item.pos}  onClick={() => handleItemclick(item)}>
                     <div className="item-contend">
-                        <img
-                            src={item.image}
-                            alt={item.food ? `Imagen de ${item.food}` : ""}
-                            loading="lazy"
-                        />
                         <div>
                             <h3 
                              className="tittle-food">
